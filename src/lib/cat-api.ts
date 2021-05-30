17e81d162120d67c1c67f0e63e3879901ff6dafa
David North <davidnorth@gmail.com>
2021-05-30 13:38:01 +0100

    Some cleanup and comments

--- src/lib/cat-api.ts
+++ src/lib/cat-api.ts
@@ -14,20 +14,25 @@ declare global
 const BASE_URL = 'https://api.thecatapi.com/v1'
 
 function getCats () {
-  // Fetch all favourites first so we can flag the favourite state on the image
-  // later. Thought the include_favourite option would mean this was included in the /images response
-  // but it isn't.  I have some thoughts on a more production quality way to handle this.
+  // Fetch all favourites first so we can store the favourite record on the image 
+  // so we can show the filled heart for cats that were favourited, and so we have the
+  // favourite record id to use when un-favouriting.
+  // I don't think this is a production quality solution by any means. It doesn't address 
+  // that favourites are paginated so if the user had favourited 1000s of cats it would be
+  // unrealistic to fetch all of those just so we can match the favourite to the image. 
+  // In a production situation I'd expect that we'd want the images request to be cached 
+  // and the user specific data to be fetched after. Then we might have an endpoint that would
+  // allow fetching the favourited state for each paginated batch of image ids. Setting the
+  // favourite state in the UI could be deffered to make everything more responsive.
+  // If we're happy that the list of cats is user specific, then the API might include the favourite 
+  // state (and votes) along with the images response. This endpoint has the option 'include_favourite' 
+  // and 'include_vote' but their purpose is undocumented and they don't alter the response in any way.
+  // I'm mindful that you might prefer an async/await solution here but this way is very natural to me.
   return superagent
     .get(BASE_URL + '/favourites')
     .set('X-API-Key', process.env.REACT_APP_API_KEY)
     .then((r) => {
-
-      console.log('favourites response:')
-      console.log(r)
-      console.log('favs:')
       const favs = r.body.map((f:Favourite) => ({id: f.id, image_id: f.image_id}))
-      console.log(favs)
-
       return superagent
         .get(BASE_URL + '/images')
         .query({limit: 100, include_vote: 1, include_favourite: 1})
@@ -35,7 +40,7 @@ function getCats () {
         .then((res) => {
           return res.body.map((cat: Cat) => {
             // Assign the matching favourite object to the cat for use in the heart widget
-            // We need the favourite id so we can un-favourite
+            // We need the favourite id so we can un-favourite. It isn't enough to just know that the image was favourited
             return Object.assign(cat, {favourite: favs.find((f:Favourite) => f.image_id === cat.id) })
           }) 
         })
