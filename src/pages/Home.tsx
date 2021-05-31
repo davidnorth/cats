ef28d57c3255ee8af4a18929f1c59ef1137223c6
David North <davidnorth@gmail.com>
2021-05-31 07:36:54 +0100

    Loading state for home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,33 @@ import React, {useState, useEffect} from 'react'
 import Cat from '../components/Cat'
 import {getCats} from '../lib/cat-api'
 import { Link } from "react-router-dom"
+import ClipLoader from "react-spinners/ClipLoader";
 import './Home.css'
 
 function Home() {  
   
   const [cats, setCats] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    getCats().then((cats) => {
-      setCats(cats)
-    })
+    getCats()
+      .then((cats) => {
+        setLoading(false)
+        setCats(cats)
+      })
+      .then((e) => {
+        // TODO: Handle error
+        console.log(e)
+      })
   }, [])
 
-  console.log(cats)
-  
   return (
     <div>
         <Link to="/upload">Upload a cat</Link>
+        <div className="contentLoading">
+          <ClipLoader loading={loading} size='75' />
+        </div>
+        {!loading && !cats.length ? <p>No cats have been uploaded yet.</p> : null}
         <div className="grid">
           {cats.map((cat:Cat) => (
             <Cat key={cat.id} cat={cat} />
